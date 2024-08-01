b6bfa08c7bfb0fbcd57dd726805d0e91518dfbb7
Dipshandc <dipshandcnew@gmail.com>
2024-08-01 19:12:33 +0545

    feat:User profile  endpoint

--- auth/urls.py
+++ auth/urls.py
@@ -3,10 +3,11 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from .views import UserViewSet
+from .views import UserViewSet, UserProfileView
 
 urlpatterns = [
     path('register/',UserViewSet.as_view(),name='register'),
+    path('profile/',UserProfileView.as_view(),name='user-profile'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- auth/views.py
+++ auth/views.py
@@ -1,11 +1,12 @@
 from django.forms import ValidationError
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from drf_spectacular.utils import extend_schema, OpenApiExample
-from .models import CustomUser
-from .serializers import UserCreateSerializer
+from .models import CustomUser,UserProfile
+from .serializers import UserCreateSerializer, UserProfileSerializer
 
 class UserViewSet(APIView):
     serializer_class = UserCreateSerializer
@@ -40,3 +41,20 @@ class UserViewSet(APIView):
 
     def get_success_headers(self, data):
         return {}
+    
+class UserProfileView(APIView):
+     serializer_class = UserProfileSerializer
+     permission_classes = [IsAuthenticated]
+     
+     def get(self,request):
+          profile = get_object_or_404(UserProfile,user=request.user)
+          serializer = self.serializer_class(profile)
+          return Response(serializer.data,status=status.HTTP_200_OK)
+     
+     def patch(self,request):
+          profile = get_object_or_404(UserProfile,user=request.user)
+          serializer = self.serializer_class(profile,data=request.data,partial=True)
+          if serializer.is_valid(raise_exception=True):
+           serializer.save()
+           return Response({"message": f"Profile's {', '.join(request.data.keys())} edited successfully"})
+          
