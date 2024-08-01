ced13d4c8703acebf967a2eef44c7c4e0bfda20e
Dipshandc <dipshandcnew@gmail.com>
2024-08-01 22:24:53 +0545

    feat:auto cart object creation on user creation

--- product/admin.py
+++ product/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Product, Category, ProductImage
+from .models import Product, Category, ProductImage, Cart
 
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(ProductImage)
+admin.site.register(Cart)
+
--- product/apps.py
+++ product/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class ProductConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product'
+
+    def ready(self):
+        from .signals.handlers import  create_cart
+        from .models import CustomUser
+        from django.db.models.signals import post_save
+        post_save.connect(create_cart, sender=CustomUser)
